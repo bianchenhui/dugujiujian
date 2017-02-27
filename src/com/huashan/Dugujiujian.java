7aeef64034a8f820a53e7d1ffc588d5f55482f24
bianchenhui00 <bianchenhui_word@126.com>
2017-02-27 23:35:43 +0800

    update 0.2

--- src/com/huashan/Dugujiujian.java
+++ src/com/huashan/Dugujiujian.java
@@ -3,7 +3,10 @@ package com.huashan;
 public class Dugujiujian {
 
 	public static void main(String[] args) {
-		System.out.println("hello,dugujiujian");
+		System.out.println("hello,dugujiujian0");
+		System.out.println("hello,dugujiujian1");
+		System.out.println("hello,dugujiujian2");
+		System.out.println("hello,dugujiujian3");
 	}
 
 }
